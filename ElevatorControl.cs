e588a73172fff99c3ae1fc1c947c89b87e599c05
Matthew King <32280053+crashpro33@users.noreply.github.com>
2019-04-30 19:03:36 -0500

    Update ElevatorControl.cs

--- ElevatorControl.cs
+++ ElevatorControl.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class ElevatorControl : MonoBehaviour {
 
-    public GameObject elevator;
-    private Vector3 elevatorOrigin;
-    private bool playerOn;
+	public GameObject elevator;
+	private Vector3 elevatorOrigin;
+	private bool playerOn;
 
 	// Use this for initialization
 	void Start () {
-        elevatorOrigin = elevator.transform.position;
+        	elevatorOrigin = elevator.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (playerOn == false && transform.position.y > elevatorOrigin.y + 2) {
-            elevator.transform.position -= elevator.transform.up * Time.deltaTime;
-        }
+        	if (playerOn == false && transform.position.y > elevatorOrigin.y + 2) {
+            		elevator.transform.position -= elevator.transform.up * Time.deltaTime;
+        	}
   
-    }
+    	}
 
-    private void OnTriggerStay(Collider other) {
-        if (other.tag.Equals("Player")) {
-            playerOn = true;
-            elevator.transform.position += elevator.transform.up * Time.deltaTime;
-        }
-    }
+	private void OnTriggerStay(Collider other) {
+		if (other.tag.Equals("Player")) {
+	    		playerOn = true;
+	    		elevator.transform.position += elevator.transform.up * Time.deltaTime;
+		}
+	}
 
-    private void OnTriggerExit(Collider other) {
-        playerOn = false;
-    }
+	private void OnTriggerExit(Collider other) {
+		playerOn = false;
+	}
 }
